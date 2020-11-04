7f00ef43c29c5fd322c3dfb796a002dbbf58478d
Nwabuzor C. Obiora <38951888+shaolinmkz@users.noreply.github.com>
2020-11-05 00:18:22 +0100

    remove api key

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const YELP_API_KEY = "WwRB_NK54giVMc_fDDsYeAwxz0cdd917ZAqFNOXQIQ6L-vjik6ZZBdWOchAg6A5FVSVyYhwT6azhbqAPxJntWdX1Hpt4Zxk7bieOqW854lk503Tq_HIIjkDBwB-ZX3Yx"
+const YELP_API_KEY = '<YOUR-API-KEY>';
 
 export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
